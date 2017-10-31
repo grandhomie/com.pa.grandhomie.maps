d4ebb3281e9c3503e83835db8531c2872e666e00
Grand Homie <grandhomie@users.noreply.github.com>
2017-10-30 21:22:48 -0700

    2 new planets

--- ui/mods/grandhomie/maps.js
+++ ui/mods/grandhomie/maps.js
@@ -16,6 +16,8 @@ cShareSystems.load_pas("Grand Homie's 3v3+ NS", [
     "coui://ui/mods/grandhomie/systems/castillon_4v4_ns.pas",
     "coui://ui/mods/grandhomie/systems/cannae_4v4_ns.pas",
     "coui://ui/mods/grandhomie/systems/bosworth_field_4v4_ns.pas",
+    "coui://ui/mods/grandhomie/systems/bridges_map_4v4_ns.pas",
+    "coui://ui/mods/grandhomie/systems/prof_x4_4v4_ns.pas",
     "coui://ui/mods/grandhomie/systems/screwball_4v4_ns.pas",
     "coui://ui/mods/grandhomie/systems/thewall_4v4_ns.pas",
     "coui://ui/mods/grandhomie/systems/kuppa_600_5v5_ns.pas",
