c6e1b6a4a3094002ce8e31dfaac5cd5f28509f95
Grand Homie <grandhomie@users.noreply.github.com>
2018-09-02 11:28:50 -0700

    added vega

--- ui/mods/grandhomie/maps.js
+++ ui/mods/grandhomie/maps.js
@@ -58,6 +58,7 @@ cShareSystems.load_pas("Grand Homie XvX / FFA", [
     "coui://ui/mods/grandhomie/systems/euphor.pas",
     "coui://ui/mods/grandhomie/systems/wadiya_2018_97-4-550.pas",
     "coui://ui/mods/grandhomie/systems/stykades_102-4-650.pas",
+    "coui://ui/mods/grandhomie/systems/vega_123-3-725.pas",
     "coui://ui/mods/grandhomie/systems/wadiya_no_crater.pas",
     "coui://ui/mods/grandhomie/systems/32_ffa.pas"
 ]);
