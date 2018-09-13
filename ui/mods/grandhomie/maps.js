cfe0e43e5808ff07c2f977907dbd9d148c10c0fe
Grand Homie <grandhomie@users.noreply.github.com>
2018-09-12 19:00:23 -0700

    lune 1v1

--- ui/mods/grandhomie/maps.js
+++ ui/mods/grandhomie/maps.js
@@ -1,5 +1,6 @@
 cShareSystems.load_pas("Grand Homie 1v1, 2v2 & 3v3", [
     "coui://ui/mods/grandhomie/systems/the_void_1v1.pas",
+    "coui://ui/mods/grandhomie/systems/lune_1v1.pas",
     "coui://ui/mods/grandhomie/systems/the_void.pas",
     "coui://ui/mods/grandhomie/systems/lune_2v2.pas",
     "coui://ui/mods/grandhomie/systems/la_somme_2v2.pas",
