30151d08b2ac13122478bdac66a1d66e05cc697d
Grand Homie <grandhomie@users.noreply.github.com>
2019-05-02 13:47:19 +0200

    adding 3 new maps

--- ui/mods/grandhomie/maps.js
+++ ui/mods/grandhomie/maps.js
@@ -7,6 +7,7 @@ cShareSystems.load_pas("Grand Homie 1v1, 2v2 & 3v3", [
     "coui://ui/mods/grandhomie/systems/the_void_1v1.pas",
     "coui://ui/mods/grandhomie/systems/lune_1v1.pas",
     "coui://ui/mods/grandhomie/systems/the_void.pas",
+    "coui://ui/mods/grandhomie/systems/glacier_4p.pas",
     "coui://ui/mods/grandhomie/systems/lune_2v2.pas",
     "coui://ui/mods/grandhomie/systems/la_somme_2v2.pas",
     "coui://ui/mods/grandhomie/systems/theia_4v4_ns.pas",
@@ -19,7 +20,8 @@ cShareSystems.load_pas("Grand Homie 1v1, 2v2 & 3v3", [
     "coui://ui/mods/grandhomie/systems/hells_pass_2v2_ns.pas",
     "coui://ui/mods/grandhomie/systems/the_reef_2v2_ns.pas",
     "coui://ui/mods/grandhomie/systems/t-_crevage_2v2_ns.pas",
-	"coui://ui/mods/grandhomie/systems/lugaan_2v2_ns.pas",
+    "coui://ui/mods/grandhomie/systems/lugaan_2v2_ns.pas",
+    "coui://ui/mods/grandhomie/systems/hydra_6p.pas",
     "coui://ui/mods/grandhomie/systems/twisted_3v3_ns.pas",
     "coui://ui/mods/grandhomie/systems/the_somme_3v3_ns.pas",
     "coui://ui/mods/grandhomie/systems/tyburn_3v3_ns.pas",
@@ -45,6 +47,7 @@ cShareSystems.load_pas("Grand Homie 4v4 & 5v5", [
     "coui://ui/mods/grandhomie/systems/lune_4v4_ns.pas",
     "coui://ui/mods/grandhomie/systems/heavy_metal_gauntlet_4v4.pas",
     "coui://ui/mods/grandhomie/systems/thewall_4v4_ns.pas",
+    "coui://ui/mods/grandhomie/systems/asphodel_meadows_5v5.pas",
     "coui://ui/mods/grandhomie/systems/station_gh_5v5_ns.pas",
     "coui://ui/mods/grandhomie/systems/bastille_5v5_ns.pas",
     "coui://ui/mods/grandhomie/systems/2_croissants_5v5_ns.pas",
@@ -64,6 +67,8 @@ cShareSystems.load_pas("Grand Homie 12+ Players", [
     "coui://ui/mods/grandhomie/systems/32_ffa.pas"
 ]);
 cShareSystems.load_pas("Grand Homie XvX / FFA", [
+    "coui://ui/mods/grandhomie/systems/glacier_4p.pas",
+    "coui://ui/mods/grandhomie/systems/hydra_6p.pas",
     "coui://ui/mods/grandhomie/systems/filibuster_6p_ffa.pas",
     "coui://ui/mods/grandhomie/systems/actarus_42-3-475.pas",
     "coui://ui/mods/grandhomie/systems/euphor.pas",
